b4022795b7cded54659d856c718cb3029183152b
bruhu <annabru.abh@gmail.com>
2020-02-18 15:17:45 +0100

    Implemented members for all three abstract functions to override

--- app/src/main/java/com/bruhu/recyclerview/NoteAdapter.kt
+++ app/src/main/java/com/bruhu/recyclerview/NoteAdapter.kt
@@ -7,8 +7,20 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 class NoteAdapter(
+    // constructor w/ 2 parameters
     private val mainActivity: MainActivity,
     private val noteList: List<Note>)
     : RecyclerView.Adapter<NoteAdapter.ListItemHolder>(){
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteAdapter.ListItemHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(holder: NoteAdapter.ListItemHolder, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 
 }
