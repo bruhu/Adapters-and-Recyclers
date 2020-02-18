6d51a0e309c7f24e0fef1d48f41eb96bd9821a7f
bruhu <annabru.abh@gmail.com>
2020-02-18 17:21:18 +0100

    Cleaned imports

--- app/src/main/java/com/bruhu/recyclerview/MainActivity.kt
+++ app/src/main/java/com/bruhu/recyclerview/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.bruhu.recyclerview
 
 import android.os.Bundle
-//import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
